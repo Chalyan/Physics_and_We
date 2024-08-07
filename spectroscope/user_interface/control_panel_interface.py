124636117d02d12a17457c45091c6f1533faafcd
Davviidh <hayrapet.david@gmail.com>
2024-08-07 12:01:06 +0400

    Control Panel Interface

---
+++ spectroscope/user_interface/control_panel_interface.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+class ControlPanelInterface(tk.Frame):
+    def __init__(self, root, style_dict):
+        self.root = root
+        self.style_dict = style_dict
+
+
--- spectroscope/user_interface/main_loop.py
+++ spectroscope/user_interface/main_loop.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 import json
 
+from control_panel_interface import ControlPanelInterface
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("YSO_spectroscope")
     with open("default_style.json", "r") as f:
         style_dict = json.load(f)
+    
 
+    panel = ControlPanelInterface(root, style_dict)
     root.mainloop()
+
