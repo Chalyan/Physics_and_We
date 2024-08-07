733ee1f6a72da6f8d0aebf7ae3dc57684dbba2a3
MilG7 <miligas06@gmail.com>
2024-08-07 17:13:37 +0400

    Added average slider

--- spectroscope/user_interface/control_panel_interface.py
+++ spectroscope/user_interface/control_panel_interface.py
@@ -3,9 +3,10 @@ import tkinter as tk
 
 class ControlPanelInterface(tk.Frame):
     def __init__(self, parent, style_dict):
-        tk.Frame.__init__(self, parent, bg = "green")
+        tk.Frame.__init__(self, parent, bg = "blue")
         self.root = parent
         self.style_dict = style_dict
-
-
+        self.AverageSlider = tk.Scale(self, from_=1, to=100,
+                                      **self.style_dict["Scale"])
+        self.AverageSlider.pack()
 
