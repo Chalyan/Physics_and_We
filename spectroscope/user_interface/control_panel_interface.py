342196a49a028b650cfbb0be953694ee47538a31
Davviidh <hayrapet.david@gmail.com>
2024-08-07 13:29:46 +0400

    Control Panel Interface

--- spectroscope/user_interface/control_panel_interface.py
+++ spectroscope/user_interface/control_panel_interface.py
@@ -2,8 +2,10 @@ import tkinter as tk
 
 
 class ControlPanelInterface(tk.Frame):
-    def __init__(self, root, style_dict):
-        self.root = root
+    def __init__(self, parent, style_dict):
+        tk.Frame.__init__(self, parent, bg = "green")
+        self.root = parent
         self.style_dict = style_dict
+
 
 
