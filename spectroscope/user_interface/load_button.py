a422d87eaa4b74f6576a16a213f6161c689006b6
Davviidh <hayrapet.david@gmail.com>
2024-08-07 18:15:39 +0400

    Added Load Button

---
+++ spectroscope/user_interface/load_button.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter import filedialog
+import csv
+
+def openFile():   
+   the_file = filedialog.askopenfilename(  # Open explorer
+      title = "Select a .csv file",  
+      filetypes = (("CSV Files","*.csv"),) # File type only csv
+      )  
+   with open(the_file, 'r') as file:
+      reader = csv.reader(file)
+      # Create graph TO DO
+      
+window = Tk()
+
+window.title('File Explorer')
+window.geometry("500x500")
+
+
+button_explore = Button(window, 
+						text = "Load",
+						command = openFile) 
+
+
+button_explore.grid()
+
+
+window.mainloop()
