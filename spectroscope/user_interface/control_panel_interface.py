370512db14fc009c0d16fb12d6bd695982caf782
Davviidh <hayrapet.david@gmail.com>
2024-08-08 10:55:20 +0400

    Created OOP

--- spectroscope/user_interface/control_panel_interface.py
+++ spectroscope/user_interface/control_panel_interface.py
@@ -1,11 +1,25 @@
 import tkinter as tk
-
+from tkinter import filedialog
+import csv
 
 class ControlPanelInterface(tk.Frame):
     def __init__(self, parent, style_dict):
         tk.Frame.__init__(self, parent, bg = "green")
         self.parent = parent
         self.style_dict = style_dict
+        self.load_button = tk.Button(self, text="Load", command=self.load_func)
+        self.load_button.pack()
+
+    def load_func(self):
+        the_file = filedialog.askopenfilename(  # Open explorer
+            title = "Select a .csv file",  
+            filetypes = (("CSV Files","*.csv"),) # File type only csv
+            )  
+        with open(the_file, 'r') as file:
+            return csv.reader(file)
+        
+
+
 
 
 
