fe509d2516cf556c78a75bdc63671a8052774469
Vahan <hy.vahan.sargsyan@gmail.com>
2024-08-08 20:46:52 +0400

    [fix] Removed unnessecary "wavelength" columns and deleted local path of zip in my computer for other's use

--- Merging_CSV_files.py
+++ Merging_CSV_files.py
@@ -1,76 +1,32 @@
 import pandas as pd
 import zipfile as zf
 import io
-import os
 from srcread import SRCRead
-
 
 zipFile = SRCRead('source.txt')
 zip_csv_file = zipFile.findPath('zip_file_path')
 
-dataframes = []
+output_buffer = io.StringIO()
+
+merged_df = None 
 
 with zf.ZipFile(zip_csv_file, 'r') as zip_ref:
     for file_name in zip_ref.namelist():
         if file_name.endswith('.csv'):
             with zip_ref.open(file_name) as csvfile:
                 df = pd.read_csv(csvfile)
-                dataframes.append(df)
-
-merged_df = pd.concat(dataframes, axis=1)
-
-output_buffer = io.StringIO()
+                
+                if merged_df is None:
+                    merged_df = df
+                else:
+                    
+                    merged_df = pd.concat([merged_df, df.iloc[:, 1:]], axis=1)
 
 merged_df.to_csv(output_buffer, index=False)
 
-output_buffer.flush()
-
-output_file_path = 'merged_output.csv' 
+output_file_path = 'merged_output.csv'
 
 with open(output_file_path, 'w') as f:
     f.write(output_buffer.getvalue())
 
 output_buffer.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# first_file = True
-
-# with zf.ZipFile(zip_csv_file, 'r') as zip_ref:
-#     for file_name in zip_ref.namelist():
-#         if file_name.endswith('.csv'):
-#             with zip_ref.open(file_name) as csvfile:
-#                 df = pd.read_csv(csvfile)                
-#                 if first_file:
-#                     df.to_csv(output_buffer, index=False)
-#                     first_file = False
-#                 else:
-#                     df.iloc[:, 1:].to_csv(output_buffer, index=False, header=False)
-
-# output_buffer.flush()
-
-# output_file_path = 'merged_output.csv'  
-
-# with open(output_file_path, 'w') as f:
-#     f.write(output_buffer.getvalue())
-
-# output_buffer.close()
